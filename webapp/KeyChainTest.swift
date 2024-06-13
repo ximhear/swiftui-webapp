a7b7af23893b6ee5069f4ab326e6fc42c37cd18e
gzonelee <gzonelee@gmail.com>
2024-06-13 16:45:48 +0900

    key chain test

---
+++ webapp/KeyChainTest.swift
@@ -0,0 +1,18 @@
+//
+//  KeyChainTest.swift
+//  webapp
+//
+//  Created by gzonelee on 6/13/24.
+//
+
+import SwiftUI
+
+struct KeyChainTest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    KeyChainTest()
+}
